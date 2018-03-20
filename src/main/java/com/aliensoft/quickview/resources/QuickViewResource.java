c6d43cf8b3f1551665dde9b0a37ac806490d73ad
Pavel-Teplitsky <pavel.teplitsky@groupdocs.com>
2018-03-20 15:32:48 +0200

    Updated version of the GroupDocs.Viewer library Updated the code to use new and updated GroupDocs.Viewer methods

--- src/main/java/com/aliensoft/quickview/resources/QuickViewResource.java
+++ src/main/java/com/aliensoft/quickview/resources/QuickViewResource.java
@@ -1,18 +1,18 @@
 package com.aliensoft.quickview.resources;
 
 import com.aliensoft.quickview.config.QuickViewConfig;
-import com.aliensoft.quickview.domain.web.MediaType;
-import com.aliensoft.quickview.domain.wrapper.ErrorMsgWrapper;
 import com.aliensoft.quickview.domain.wrapper.FileDescriptionWrapper;
 import com.aliensoft.quickview.views.QuickView;
 import com.groupdocs.viewer.config.ViewerConfig;
 import com.groupdocs.viewer.converter.options.HtmlOptions;
 import com.groupdocs.viewer.domain.FileDescription;
 import com.groupdocs.viewer.domain.containers.DocumentInfoContainer;
-import com.groupdocs.viewer.domain.containers.FileTreeContainer;
+import com.groupdocs.viewer.domain.containers.FileListContainer;
 import com.groupdocs.viewer.domain.options.DocumentInfoOptions;
-import com.groupdocs.viewer.domain.options.FileTreeOptions;
 import com.groupdocs.viewer.handler.ViewerHtmlHandler;
+import com.aliensoft.quickview.domain.web.MediaType;
+
+import com.aliensoft.quickview.domain.wrapper.ErrorMsgWrapper;
 import com.groupdocs.viewer.licensing.License;
 
 import javax.servlet.http.HttpServletRequest;
@@ -75,12 +75,17 @@ public class QuickViewResource extends QuickViewResourcesBase{
         String requestBody = getRequestBody(request);
         String relDirPath = getJsonString(requestBody, "path");
         // get file list from storage path
-        FileTreeOptions fileTreeOptions = new FileTreeOptions(relDirPath);
-        FileTreeContainer fileTreeContainer = viewerHtmlHandler.loadFileTree(fileTreeOptions);
+        //FileTreeOptions fileTreeOptions = new FileTreeOptions(relDirPath);
+        FileListContainer fileTreeContainer = null;
+        try {
+            fileTreeContainer = viewerHtmlHandler.getFileList();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         ArrayList<FileDescriptionWrapper> fileList = new ArrayList<>();
         // parse file lists
-        for(FileDescription fd : fileTreeContainer.getFileTree()){
+        for(FileDescription fd : fileTreeContainer.getFiles()){
             FileDescriptionWrapper fileDescription = new FileDescriptionWrapper();
             fileDescription.setGuid(fd.getGuid());
             fileDescription.setName(fd.getName());
@@ -104,11 +109,13 @@ public class QuickViewResource extends QuickViewResourcesBase{
         // set response content type
         setResponseContentType(response, MediaType.APPLICATION_JSON);
         try {
-            //set parameters
-            String documentGuid = "/Users/Alex/Documents/GroupDocs/java-codeconventions.pdf";
+            // get request body
+            String requestBody = getRequestBody(request);
+            // set parameters
+            String documentGuid = getJsonString(requestBody, "guid");
             // get/set document description
             DocumentInfoOptions documentInfoOptions = new DocumentInfoOptions(documentGuid);
-            DocumentInfoContainer documentInfoContainer = viewerHtmlHandler.getDocumentInfo(documentInfoOptions);
+            DocumentInfoContainer documentInfoContainer = viewerHtmlHandler.getDocumentInfo(documentGuid, documentInfoOptions);
             // return document description
             return objectToJson(documentInfoContainer.getPages());
         }catch (Exception ex){
@@ -131,12 +138,13 @@ public class QuickViewResource extends QuickViewResourcesBase{
             String documentGuid = getJsonString(requestBody, "guid");
             // get/set document description
             DocumentInfoOptions documentInfoOptions = new DocumentInfoOptions(documentGuid);
-            System.out.println(documentInfoOptions.getCellsDocumentInfoOptions());
-            System.out.println(documentInfoOptions.getEmailDocumentInfoOptions());
-            System.out.println(documentInfoOptions.getGuid());
+            DocumentInfoContainer documentInfoContainer = viewerHtmlHandler.getDocumentInfo(documentGuid, documentInfoOptions);
+            System.out.println(documentInfoOptions.getCellsOptions());
+            System.out.println(documentInfoOptions.getEmailOptions());
+            System.out.println(documentInfoContainer.getGuid());
             System.out.println(documentInfoOptions.getPassword());
-            System.out.println(documentInfoOptions.getWordsDocumentInfoOptions());
-            DocumentInfoContainer documentInfoContainer = viewerHtmlHandler.getDocumentInfo(documentInfoOptions);
+            System.out.println(documentInfoOptions.getWordsOptions());
+
             // return document description
             return objectToJson(documentInfoContainer.getPages());
         }catch (Exception ex){
@@ -158,14 +166,16 @@ public class QuickViewResource extends QuickViewResourcesBase{
         try {
             // set response content type
             setResponseContentType(response, MediaType.TEXT_HTML);
-            // set parameters
-            String documentGuid = "/Users/Alex/Documents/GroupDocs/java-codeconventions.pdf";
+            // get request body
+            String requestBody = getRequestBody(request);
+            // get/set parameters
+            String documentGuid = getJsonString(requestBody, "guid");
             int pageNumber = 1;
             int countPagesToConvert = 1;
             // set options
             HtmlOptions htmlOptions = new HtmlOptions();
             htmlOptions.setPageNumber(pageNumber);
-            htmlOptions.setCountPagesToConvert(countPagesToConvert);
+            htmlOptions.setCountPagesToRender(countPagesToConvert);
             htmlOptions.setResourcesEmbedded(true);
             // return html
             return viewerHtmlHandler.getPages(documentGuid, htmlOptions).get(0).getHtmlContent();
@@ -193,7 +203,7 @@ public class QuickViewResource extends QuickViewResourcesBase{
             // set options
             HtmlOptions htmlOptions = new HtmlOptions();
             htmlOptions.setPageNumber(pageNumber);
-            htmlOptions.setCountPagesToConvert(1);
+            htmlOptions.setCountPagesToRender(1);
             htmlOptions.setResourcesEmbedded(true);
             // return html
             return viewerHtmlHandler.getPages(documentGuid, htmlOptions).get(0).getHtmlContent();
